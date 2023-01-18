ab0b5a415f06a2a8e946ff2d1687e125c7f4a4da
Ultra-tech-code <hizick27@gmail.com>
2023-01-18 12:13:05 +0100

    setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-etherscan");
+import "@nomiclabs/hardhat-ethers";
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
+const ALCHEMY_mumbai_API_KEY_URL = process.env.ALCHEMY_mumbai_API_KEY_URL;
+//contract address key
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_mumbai_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
-
-export default config;
